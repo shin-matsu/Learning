1c4a8e5f9abb0f6032f0338b951b08ddf01f8f18
shin-matsu <48970312+shin-matsu@users.noreply.github.com>
2019-04-30 21:02:18 +0900

    グレースケール
    読み込んだ画像をグレースケールの処理追加

--- OCR/example.py
+++ OCR/example.py
@@ -38,10 +38,10 @@ open_image = Image.open(input_image)
 # グレースケール変換関数
 def ConversionGrayScale(img):
     gray_img = img.convert('L')
+    gray_img.save('PIL.jpg')
     return gray_img
 
 grayscale_image = ConversionGrayScale(open_image)
-
 #文字読み込み
 txt = tool.image_to_string(
     grayscale_image,
@@ -59,12 +59,17 @@ res = tool.image_to_string(
 #print( res )
 
 #文字として認識した場所を矩型で表示
-out = cv2.imread(input_image)
+out = cv2.imread(input_image, 1)
+
+#resized_out = cv2.resize(out,(width//4, height//4))
+
 for d in res:
     #print(d.content)
     #print(d.position)
     cv2.rectangle(out, d.position[0], d.position[1], (0, 255, 0), 2)
 
+
+cv2.namedWindow("img", cv2.WINDOW_NORMAL)
 cv2.imshow("img",out)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
